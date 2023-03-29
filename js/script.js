6e86b4da28e7c9c47b19909af3ab57209332eb66
Sherab-Z <codebodhi34@gmail.com>
2023-03-29 22:30:01 +0200

    Create operate() func

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
 // OPERATION VARS
 const a = 0;
-const op = '';
+const op = "";
 const b = 0;
 
 // Operator Functions:
-// FUNC: sum() input 2 numbers, return their sum
-function add(a, b) {
-  return (isNaN(a) || isNaN(b)) ? prompt("Error: Input is not a number") : a + b;
+// FUNC: add a and b, return the result
+function plus(a, b) {
+  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a + b;
 }
 
-// FUNC: subtract() input 2 numbers, return their difference
+// FUNC: subtract b from a, return the result
 function subtract(a, b) {
-  return (isNaN(a) || isNaN(b)) ? prompt("Error: Input is not a number") : a - b;
+  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a - b;
 }
 
-// FUNC: multiply() input 2 numbers, return their multiplication
-function multiply(a, b) {
-  return (isNaN(a) || isNaN(b)) ? prompt("Error: Input is not a number") : a * b;
+// FUNC: multiply a and b, return the result
+function multipliedBy(a, b) {
+  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a * b;
 }
 
-// FUNC: divide() input 2 numbers, return their division
-function divide(a, b) {
-  return (isNaN(a) || isNaN(b)) ? prompt("Error: Input is not a number") : a / b;
+// FUNC: divide a by b, return the result
+function dividedBy(a, b) {
+  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a / b;
 }
 
-
+function operate(a, op, b) {
+  if (
+    typeof a === "number" &&
+    typeof b === "number" &&
+    typeof op === "string"
+  ) {
+    switch (op) {
+      case "add":
+        return add(a, b);
+      case "sub":
+        return sub(a, b);
+      case "mul":
+        return mul(a, b);
+      case "div":
+        return div(a, b);
+      default:
+        console.error(op + " is not a valid operator");
+    }
+  } else {
+    console.error("Invalid input: Wrong data type");
+  }
+}
