41173fe2573163b0cc955c4aa0a628495888c02c
Sherab-Z <codebodhi34@gmail.com>
2023-03-31 22:44:12 +0200

    Encapsulate vars in varObj, refactor type-checks; refactor html classes

--- js/script.js
+++ js/script.js
@@ -1,48 +1,72 @@
 // OPERATION VARS
-const a = 0;
-const op = "";
-const b = 0;
+const varObj = {
+  a : 0,
+  op : "",
+  b : 0,
+};
 
 // Operator Functions:
 // FUNC: add a and b, return the result
 function plus(a, b) {
-  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a + b;
+  // Update properties in varObj
+  varObj.a = a;
+  varObj.b = b;
+
+  // Return the sum.
+  return a + b;
 }
 
-// FUNC: subtract b from a, return the result
-function subtract(a, b) {
-  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a - b;
+// FUNC: minus b from a, return the result
+function minus(a, b) {
+  // Update properties in varObj
+  varObj.a = a;
+  varObj.b = b;
+
+  // Return the result.
+  return a - b;
 }
 
 // FUNC: multiply a and b, return the result
 function multipliedBy(a, b) {
-  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a * b;
+  // Update properties in varObj
+  varObj.a = a;
+  varObj.b = b;
+
+  // Return the result.
+  return a * b;
 }
 
 // FUNC: divide a by b, return the result
 function dividedBy(a, b) {
-  return isNaN(a) || isNaN(b) ? prompt("Error: Input is not a number") : a / b;
+  // Update properties in varObj
+  varObj.a = a;
+  varObj.b = b;
+
+  // Return the result.
+  return a / b;
 }
 
 function operate(a, op, b) {
+  //  Type-check arguments
   if (
     typeof a === "number" &&
-    typeof b === "number" &&
-    typeof op === "string"
+    typeof op === "string" &&
+    typeof b === "number"
   ) {
+    //  Depending on the operator (op) passed in, call the appropriate operation function on a and b.
     switch (op) {
-      case "add":
-        return add(a, b);
-      case "sub":
-        return sub(a, b);
-      case "mul":
-        return mul(a, b);
-      case "div":
-        return div(a, b);
+      case "+":
+        return plus(a, b);
+      case "-":
+        return minus(a, b);
+      case "*":
+        return multipliedBy(a, b);
+      case "/":
+        return dividedBy(a, b);
       default:
-        console.error(op + " is not a valid operator");
+        console.error(`${op} is not a valid operator`);
     }
-  } else {
-    console.error("Invalid input: Wrong data type");
+  } else {  // If type-check fails, log an error to the console.
+    console.error(`Invalid input: Wrong data type`);
   }
 }
